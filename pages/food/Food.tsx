a0e58cf30d6dcca853e3e17128da50d80822c60a
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-14 17:38:20 +0800

    added search bar that filters the food list

--- pages/food/Food.tsx
+++ pages/food/Food.tsx
@@ -1,18 +1,36 @@
 import styles from "./Food.module.css";
 
-import { FC, useMemo } from "react";
+import { FC, useMemo, useState } from "react";
 import foodData from "../../data/foodData";
 import { FoodType } from "../../data/foodData";
 
 import FoodCard from "./foodCard/FoodCard";
 
 const Food: FC = () => {
+  const [searchFoodListInput, setSearchFoodListInput] = useState("");
+
+  const handleSearchFoodListInput = (query: string) => {
+    const searchResults = [...foodData].filter((FoodItem) =>
+      FoodItem.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return searchResults;
+  };
+
   const foodList: FoodType[] = useMemo(() => {
-    return [...foodData];
-  }, []);
+    const searchResults = handleSearchFoodListInput(searchFoodListInput);
+
+    return searchResults;
+  }, [searchFoodListInput]);
 
   return (
     <div className={styles.section}>
+      <input
+        type="text"
+        value={searchFoodListInput}
+        onChange={(e) => setSearchFoodListInput(e.target.value)}
+        placeholder="Search food here..."
+      />
       {foodList.map((foodItem) => (
         <FoodCard key={foodItem.id} foodItem={foodItem} />
       ))}
