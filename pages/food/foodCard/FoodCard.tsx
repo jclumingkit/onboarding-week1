daf93c8db98de16fcb111af49eac316537b397f5
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-16 18:21:38 +0800

    debug: foodItem props returns undefined on build

--- pages/food/foodCard/FoodCard.tsx
+++ pages/food/foodCard/FoodCard.tsx
@@ -5,24 +5,22 @@ import Image from "next/image";
 import { FoodType } from "../../../data/foodData";
 
 const FoodCard: FC<{ foodItem: FoodType }> = ({ foodItem }) => {
-  const rating = new Array(foodItem.rating).fill("⭐");
-
   return (
     <div className={styles.card}>
       <div>
         <Image
           priority
-          src={foodItem.image}
+          src={foodItem?.image}
           width={600}
           height={400}
-          alt={foodItem.name}
+          alt={foodItem?.name}
         />
-        <h2>{foodItem.name}</h2>
+        <h2>{foodItem?.name}</h2>
         <p>
-          <span className={styles.description}>{foodItem.description}</span>
+          <span className={styles.description}>{foodItem?.description}</span>
         </p>
       </div>
-      <h1>{rating}</h1>
+      <h1>{Array(foodItem?.rating).fill("⭐")}</h1>
     </div>
   );
 };
