e47eb6e3ba120d991ff74313fe56d11e256f4496
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-25 09:49:33 +0800

    added Google and Facebook Sign In

--- components/user/SignInPage.tsx
+++ components/user/SignInPage.tsx
@@ -2,9 +2,29 @@ import { FC, useState } from "react";
 import { Container, Stack, Text, Button, SimpleGrid } from "@mantine/core";
 import SignInForm from "./SignInForm";
 import MagicLinkForm from "./MagicLinkForm";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const SignInPage: FC = () => {
   const [showForm, setShowForm] = useState(false);
+  const supabase = useSupabaseClient();
+
+  const handleGoogleSignin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    console.log(data);
+    console.log(error);
+  };
+
+  const handleFacebookSignin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "facebook",
+    });
+
+    console.log(data);
+    console.log(error);
+  };
 
   return (
     <Container style={{ height: "100vh" }}>
@@ -27,6 +47,20 @@ const SignInPage: FC = () => {
           onClick={() => setShowForm(!showForm)}
         >
           {showForm ? "Sign In with Password" : "Sign In with Magic Link"}
+        </Button>
+        <Button
+          variant="outline"
+          type="button"
+          onClick={() => handleGoogleSignin()}
+        >
+          Google
+        </Button>
+        <Button
+          variant="outline"
+          type="button"
+          onClick={() => handleFacebookSignin()}
+        >
+          Facebook
         </Button>
       </SimpleGrid>
     </Container>
